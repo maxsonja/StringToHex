a554881a612eb32ae75377532d19b9940d58b8de
maxsonja <burn.excess@gmail.com>
2013-06-04 18:38:06 +0200

    Update and rename .gitignore to toHex.ino

--- toHex.ino
+++ toHex.ino
@@ -1,3 +1,4 @@
+//converts one hex to binary
 byte charToInt(char input)
 {
   if(input >47 && input<58) return input-48;
@@ -9,11 +10,12 @@ byte toHex(String input)
 {
   input.toUpperCase();
   byte result = 0x00;
+  //input checking
   if(input.length()==4) input = input.substring(2);
   else if(input.length()==3||input.length()>4) return Ox00;
-  
-  result=charToInt(input.charAt(0));
-  result=result<<4;
-  result+=charToInt(input.charAt(1));
+  //
+  result=charToInt(input.charAt(0));    //first hex
+  result=result<<4;                     //bitshift 4 places to the left
+  result+=charToInt(input.charAt(1));   //second hex
   return result;
 }

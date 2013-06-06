22c140af338dd3fcd23fd490479d2b43ce143978
Mario <burn.excess@gmail.com>
2013-06-06 19:07:56 +0300

    Update Convert.h

--- Convert/Convert.h
+++ Convert/Convert.h
@@ -1,11 +1,14 @@
 #ifndef Convert_h
 #define Convert_h
-class SLib
+
+#include "Arduino.h"
+
+class Convert
 {
 	public:
-		byte intValue(char)
-		byte toHex(String)
-		float toFloat(String)
-		long toInt(String)
+		byte intValue(char);
+		byte toHex(String);
+		float toFloat(String);
+		long toInt(String);
 };
 #endif
